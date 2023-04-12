8a467f69e79cff190e0737ebd929a4a70b23b75e
Myongjin <audwls714@gmail.com>
2023-04-12 23:29:39 +0900

    [ADD] Arrays

--- ShinMyongJin/VanillaJS/app.js
+++ ShinMyongJin/VanillaJS/app.js
@@ -1,3 +1,21 @@
-const amIFat = null;
-let something;
-console.log(something, amIFat);
+// const mon = 'mon';
+// const tue = 'tue';
+// const wed = 'wed';
+// const thu = 'thu';
+// const fri = 'fri';
+// const sat = 'sat';
+// const sun = 'sun';
+
+// const dayOfWeek = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat'];
+
+// //Get Item from Array
+// console.log(dayOfWeek[4]);
+
+// //Add one more day to the array
+// dayOfWeek.push('sun');
+
+// console.log(dayOfWeek);
+
+const toBuy = ['potato', 'tomato', 'pizza'];
+
+console.log(toBuy[2]);
