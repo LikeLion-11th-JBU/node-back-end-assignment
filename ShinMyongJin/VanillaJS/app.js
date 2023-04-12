06a0cfb389fbdda5c3eec6e7b906a73cfa5092a0
Myongjin <audwls714@gmail.com>
2023-04-12 23:04:30 +0900

    [ADD] const and let

--- ShinMyongJin/VanillaJS/app.js
+++ ShinMyongJin/VanillaJS/app.js
@@ -1,8 +1,12 @@
 const a = 5;
 const b = 2;
-const myName = 'mj';
+let myName = 'mj';
 
 console.log(a + b);
 console.log(a * b);
 console.log(a / b);
 console.log('hello ' + myName);
+
+myName = 'MJshin';
+
+console.log('your new name is ' + myName);
