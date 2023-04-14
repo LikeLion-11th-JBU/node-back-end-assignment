5346ebfff1181c1a58687c34ad2a994b25363b3d
seo jang seok <seok7197@naver.com>
2023-04-15 04:20:08 +0900

    2.8-function2

--- SeoJangSeok/Vanilla-JS-chrome-app/app.js
+++ SeoJangSeok/Vanilla-JS-chrome-app/app.js
@@ -1,18 +1,31 @@
 /* function */
 
-// function이 없을 때
-// console.log("Hello my name is Nico");
-// console.log("Hello my name is Dal");
-// console.log("Hello my name is Shigatsu");
-// console.log("Hello my name is Flynn");
-// 바뀌어야 할 부분은 끝의 이름들이다.
-
-function sayHello() {
-  //실행하게 될 코드 블록
-  console.log('Hello!')
+/* function sayHello(nameOfPerson, age) {
+  console.log('Hello my name is ' + nameOfPerson + "and I'm " + age) // Hello my name is nicoand I'm 10 \n Hello my name is daland I'm 23 \n Hello my name is lynnand I'm 21
 }
-// sayHello라는 function 실행
-sayHello() // Hello!
-sayHello() // Hello!
-sayHello() // Hello!
-sayHello() // Hello!
+
+sayHello('nico', 10)
+sayHello('dal', 23)
+sayHello('lynn', 21)
+*/
+
+// 계산기
+/* function plus(firstNumber, secondNumber) {
+  console.log(firstNumber + secondNumber) // plus()일때, NaN : Not a Number(숫자가 아님) // 68
+}
+function divide(a, b) {
+  console.log(a / b) // 4.9
+}
+plus(8, 60)
+divide(98, 20)
+*/
+
+const player = {
+  name: 'nico',
+  sayHello: function (otherPersonName) {
+    console.log('hello ' + otherPersonName + ' nice to meet you!') //hello nico nice to meet you! \n hello lynn nice to meet you!
+  },
+}
+
+player.sayHello('nico')
+player.sayHello('lynn')
