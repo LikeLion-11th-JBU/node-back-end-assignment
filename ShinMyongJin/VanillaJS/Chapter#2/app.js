7cd467549ccaad178a3423c3207181917fc8d57a
Myongjin <audwls714@gmail.com>
2023-04-15 00:00:51 +0900

    [ADD] 챕터2 끝

--- ShinMyongJin/VanillaJS/Chapter#2/app.js
+++ ShinMyongJin/VanillaJS/Chapter#2/app.js
@@ -91,16 +91,18 @@
 
 // console.log(krAge);
 
-const age = parseInt(prompt('How old are you?'));
+// const age = parseInt(prompt('How old are you?'));
 
-if (isNaN(age) || age < 0) {
-  console.log('Please write a real positive number');
-} else if (age < 19) {
-  console.log('You are too young');
-} else if (age >= 19 && age <= 50) {
-  console.log('You can drink');
-} else if (age > 50 && age <= 80) {
-  console.log('You should exercise');
-} else if (age > 80) {
-  console.log('You can do whatever you want');
-}
+// if (isNaN(age) || age < 0) {
+//   console.log('Please write a real positive number');
+// } else if (age < 19) {
+//   console.log('You are too young');
+// } else if (age >= 19 && age <= 50) {
+//   console.log('You can drink');
+// } else if (age > 50 && age <= 80) {
+//   console.log('You should exercise');
+// } else if (age > 80) {
+//   console.log('You can do whatever you want');
+// } else if (age === 100) {
+//   console.log('wow you are wise');
+// }
