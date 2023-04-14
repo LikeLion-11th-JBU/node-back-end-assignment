70f7bedcda453e1b9f1427bdf578a5d51c65e40b
seo jang seok <seok7197@naver.com>
2023-04-15 02:31:49 +0900

    2.6-Objects

--- SeoJangSeok/Vanilla-JS-chrome-app/app.js
+++ SeoJangSeok/Vanilla-JS-chrome-app/app.js
@@ -1,27 +1,21 @@
-/* Arrays */
+/* Object */
+// object
+const player = {
+  name: 'Jang Seok',
+  points: 10,
+  fat: true,
+}
+// object 접근 방법
+console.log(player) // == {name: 'Jang Seok', points: 10, fat: true}
+console.log(player.name) // == Jang Seok
+console.log(player['name']) // == Jang Seok
 
-// 배열이 없는 세상의 JS
-// const mon = 'mon' // String
-// const tue = 'tue' // String
-// const wed = 'wed' // String
-// const thu = 'thu' // String
-// const fri = 'fri' // String
-// const sat = 'sat' // String
-// const sun = 'sund' // String
+// Object update
+player.fat = false
+console.log(player) // == {name: 'Jang Seok', points: 10, fat: false}
+player.points = player.points + 15
+console.log(player.points) // == 25
 
-// const daysOfWeek = mon + tue + wed + thu + fri + sat + sun
-// console.log(daysOfWeek) // == montuewedthufirsatsund 문자열 형식으로 나온다.
-
-// 배열로 정렬
-// const daysOfWeek1 = [mon, tue, wed, thu, fri, sat, sun] // string 대신에 array가 된다.
-const daysOfWeek2 = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat']
-
-// console.log(daysOfWeek1) // == ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sund']
-console.log(daysOfWeek2) // == ['mon', 'tue', 'wed', 'thu', 'fri', 'sat']
-// 배열에서 아이템 받아오기
-console.log(daysOfWeek2[5]) // == sat
-console.log(daysOfWeek2[4]) // == fri
-
-// 배열에 요일 더 추가하기
-daysOfWeek2.push('sun')
-console.log(daysOfWeek2) // == ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun']
+// Object add
+player.lastName = 'potato'
+console.log(player) // == {name: 'Jang Seok', points: 10, fat: false, lastName: 'potato'}
