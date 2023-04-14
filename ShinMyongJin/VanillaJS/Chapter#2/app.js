e669ed0ec6d39cc125531bbb5a2647cdad4abfa1
Myongjin <audwls714@gmail.com>
2023-04-14 23:24:58 +0900

    [ADD] Conditionals

--- ShinMyongJin/VanillaJS/Chapter#2/app.js
+++ ShinMyongJin/VanillaJS/Chapter#2/app.js
@@ -58,29 +58,29 @@
 // player.sayHello('lynn');
 // player.sayHello('MJ');
 
-const calculator = {
-  plus: function (a, b) {
-    return a + b;
-  },
-  minus: function (a, b) {
-    return a - b;
-  },
-  times: function (a, b) {
-    return a * b;
-  },
-  divide: function (a, b) {
-    return a / b;
-  },
-  power: function (a, b) {
-    return a ** b;
-  },
-};
+// const calculator = {
+//   plus: function (a, b) {
+//     return a + b;
+//   },
+//   minus: function (a, b) {
+//     return a - b;
+//   },
+//   times: function (a, b) {
+//     return a * b;
+//   },
+//   divide: function (a, b) {
+//     return a / b;
+//   },
+//   power: function (a, b) {
+//     return a ** b;
+//   },
+// };
 
-const plusResult = calculator.plus(2, 3);
-const minusResult = calculator.minus(plusResult, 10);
-const timesResult = calculator.times(10, minusResult);
-const divideResult = calculator.divide(timesResult, plusResult);
-const powerResult = calculator.power(divideResult, minusResult);
+// const plusResult = calculator.plus(2, 3);
+// const minusResult = calculator.minus(plusResult, 10);
+// const timesResult = calculator.times(10, minusResult);
+// const divideResult = calculator.divide(timesResult, plusResult);
+// const powerResult = calculator.power(divideResult, minusResult);
 
 // const age = 96;
 // function calculateKrAge(ageOfForeigner) {
@@ -90,3 +90,7 @@ const powerResult = calculator.power(divideResult, minusResult);
 // const krAge = calculateKrAge(age);
 
 // console.log(krAge);
+
+const age = parseInt(prompt('How old are you?'));
+
+console.log(age);
