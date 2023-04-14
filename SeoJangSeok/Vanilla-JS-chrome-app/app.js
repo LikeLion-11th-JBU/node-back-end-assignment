3b0791f188df08de40c0e94598aa3571fdcf1434
seo jang seok <seok7197@naver.com>
2023-04-15 01:07:22 +0900

    2.5-Arrays

--- SeoJangSeok/Vanilla-JS-chrome-app/app.js
+++ SeoJangSeok/Vanilla-JS-chrome-app/app.js
@@ -1,12 +1,27 @@
-/* Boolean */
-//const amIFat = true //or false // true와 false를 쓸 때는 String 형식으로 입력하지 않는다.
+/* Arrays */
 
-//console.log(amIFat) // == true
+// 배열이 없는 세상의 JS
+// const mon = 'mon' // String
+// const tue = 'tue' // String
+// const wed = 'wed' // String
+// const thu = 'thu' // String
+// const fri = 'fri' // String
+// const sat = 'sat' // String
+// const sun = 'sund' // String
 
-/* null */
-const amIFat = null // amIFat에 값이 없다는 뜻
-/* undefined */
-let something
+// const daysOfWeek = mon + tue + wed + thu + fri + sat + sun
+// console.log(daysOfWeek) // == montuewedthufirsatsund 문자열 형식으로 나온다.
 
-console.log(amIFat) // == null
-console.log(something) // == undefined
+// 배열로 정렬
+// const daysOfWeek1 = [mon, tue, wed, thu, fri, sat, sun] // string 대신에 array가 된다.
+const daysOfWeek2 = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat']
+
+// console.log(daysOfWeek1) // == ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sund']
+console.log(daysOfWeek2) // == ['mon', 'tue', 'wed', 'thu', 'fri', 'sat']
+// 배열에서 아이템 받아오기
+console.log(daysOfWeek2[5]) // == sat
+console.log(daysOfWeek2[4]) // == fri
+
+// 배열에 요일 더 추가하기
+daysOfWeek2.push('sun')
+console.log(daysOfWeek2) // == ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun']
