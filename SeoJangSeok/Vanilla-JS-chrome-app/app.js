46eb4f29d38fc5ba9d68f88f6543f40a7f4759c1
seo jang seok <seok7197@naver.com>
2023-04-15 19:52:49 +0900

    2.13-Conditionals1

--- SeoJangSeok/Vanilla-JS-chrome-app/app.js
+++ SeoJangSeok/Vanilla-JS-chrome-app/app.js
@@ -1,32 +1,9 @@
-/* return */
-const age = 96
-function calcualteKrAge(ageOfForeigner) {
-  return ageOfForeigner + 2 // 98
-}
+/* 조건문1 */
+/* 음주 가능 나이 계산기 */
 
-const krAge = calcualteKrAge(age) //96 //98
-console.log(krAge) // 98
+// 유저의 나이 묻기
+//const age = prompt('How old are you?') // prompt() : 사용자에게 창을 띄울 수 있도록 한다.
+//console.log(age, parseInt(age))
 
-const calculator = {
-  add: function (a, b) {
-    return a + b
-  },
-  minus: function (a, b) {
-    return a - b
-  },
-  div: function (a, b) {
-    return a / b
-  },
-  powerOf: function (a, b) {
-    return a ** b
-  },
-  multi: function (a, b) {
-    return a * b
-  },
-}
-const addResult = calculator.add(2, 3) // 5
-const minusReuslt = calculator.minus(addResult, 10) // -5
-const multiResult = calculator.multi(10, minusReuslt) // -50
-const divResult = calculator.div(multiResult, addResult) // -10
-const powerOfResult = calculator.powerOf(divResult, minusReuslt) // -0/0000099..
-// console.log()로 결과를 확인하는 것이 아니라 addReuslt, minusResult..등을 콘솔이 아닌 화면에서 결과를 출력할 수 있다.
+const age = parseInt(prompt('How old are you?')) // 함수는 안에서 바깥으로 실행된다.
+console.log(age)
