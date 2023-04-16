11f79af8a1a390241be6ca9df78dfdda0eb17fe2
seo jang seok <seok7197@naver.com>
2023-04-17 02:24:25 +0900

    3.9-chapter3Fin

--- SeoJangSeok/Vanilla-JS-chrome-app/app.js
+++ SeoJangSeok/Vanilla-JS-chrome-app/app.js
@@ -1,12 +1,13 @@
 const h1 = document.querySelector('div.hello:first-child h1') // hello라는 class를 가진 div 내부의 first-child인 h1
 
 function handleTitleClick() {
-  const clickedClass = 'clicked sexy-font'
-  if (h1.className === clickedClass) {
-    h1.className = ''
-  } else {
-    h1.className = clickedClass
-  }
+  // const clickedClass = "clicked"
+  // if (h1.classList.contains(clickedClass)) {
+  //     h1.classList.remove(clickedClass)
+  //   } else {
+  //     h1.classList.add(clickedClass)
+  //   }
+  // }
+  h1.classList.toggle('clicked')
 }
-
 h1.addEventListener('click', handleTitleClick)
