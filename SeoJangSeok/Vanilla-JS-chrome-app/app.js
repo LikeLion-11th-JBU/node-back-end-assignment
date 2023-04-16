27ffd2d55547bb44e971eef0f01f975d219d476a
seo jang seok <seok7197@naver.com>
2023-04-17 00:11:39 +0900

    3.4-event2

--- SeoJangSeok/Vanilla-JS-chrome-app/app.js
+++ SeoJangSeok/Vanilla-JS-chrome-app/app.js
@@ -2,8 +2,20 @@ const title = document.querySelector('div.hello:first-child h1') // hello라는 
 // console.log(title) // <h1>Hello</h1>
 // console.dir(title) // object로 표시한 element 표시
 // title.style.color = 'blue'
+console.dir(title)
+
 function handleTitleClick() {
   console.log('title was clicked!')
   title.style.color = 'blue'
 }
+function handleMouseEnter() {
+  //   console.log('mouse is here!')
+  title.innerText = 'Mouse is here!'
+}
+function handleMouseLeave() {
+  title.innerText = 'Mouse is gone!'
+}
+
 title.addEventListener('click', handleTitleClick)
+title.addEventListener('mouseenter', handleMouseEnter)
+title.addEventListener('mouseleave', handleMouseLeave)
