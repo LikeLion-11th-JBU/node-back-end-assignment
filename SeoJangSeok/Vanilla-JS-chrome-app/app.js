b1721bec20e8f32781260c29f2a0e99270bcd871
seo jang seok <seok7197@naver.com>
2023-04-16 23:02:02 +0900

    3.3-event

--- SeoJangSeok/Vanilla-JS-chrome-app/app.js
+++ SeoJangSeok/Vanilla-JS-chrome-app/app.js
@@ -1,3 +1,9 @@
-const title = document.querySelector('.hello h1')
-console.log(title) // <h1>Hello</h1>
-title.innerText = 'Hello'
+const title = document.querySelector('div.hello:first-child h1') // hello라는 class를 가진 div 내부의 first-child인 h1
+// console.log(title) // <h1>Hello</h1>
+// console.dir(title) // object로 표시한 element 표시
+// title.style.color = 'blue'
+function handleTitleClick() {
+  console.log('title was clicked!')
+  title.style.color = 'blue'
+}
+title.addEventListener('click', handleTitleClick)
