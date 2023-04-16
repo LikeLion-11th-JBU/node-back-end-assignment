1695173bd4bd2d128efc97af7c2b6ff9e492a91d
Myongjin <audwls714@gmail.com>
2023-04-16 18:00:30 +0900

    [ADD] Events

--- ShinMyongJin/VanillaJS/Chapter#3/app.js
+++ ShinMyongJin/VanillaJS/Chapter#3/app.js
@@ -4,6 +4,30 @@
 
 // console.log(title.className);
 
-const title = document.querySelector('#hello');
+// const title = document.querySelector('#hello');
+// const title = document.querySelectorAll('#hello');
 
-console.log(title);
+// const title = document.querySelector('.hello:first-child h1');
+
+// console.log(title);
+
+// title.style.color = 'blue';
+
+const title = document.querySelector('div.hello:first-child h1');
+
+console.dir(title);
+
+function handleTitleClick() {
+  title.style.color = 'blue';
+}
+function handleMouseEnter() {
+  title.innerText = 'Mouse is here';
+}
+
+function handleMouseLeave() {
+  title.innerText = 'Mouse is gone';
+}
+
+title.addEventListener('click', handleTitleClick);
+title.addEventListener('mouseenter', handleMouseEnter);
+title.addEventListener('mouseleave', handleMouseLeave);
