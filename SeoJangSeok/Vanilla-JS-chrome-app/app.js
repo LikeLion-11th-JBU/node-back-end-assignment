469e9987b4faf88b06389b49c529a58913316dc9
seo jang seok <seok7197@naver.com>
2023-04-16 18:23:21 +0900

    3.2-querySelcetor

--- SeoJangSeok/Vanilla-JS-chrome-app/app.js
+++ SeoJangSeok/Vanilla-JS-chrome-app/app.js
@@ -1,6 +1,3 @@
-const title = document.getElementById('title')
-
-title.innerText = ' Got you!'
-
-console.log(title.id)
-console.log(title.className)
+const title = document.querySelector('.hello h1')
+console.log(title) // <h1>Hello</h1>
+title.innerText = 'Hello'
