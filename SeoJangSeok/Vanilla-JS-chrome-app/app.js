5003c2d81ff3b0ed0533df15814c6a379259dd84
seo jang seok <seok7197@naver.com>
2023-04-17 20:18:12 +0900

    4.0-Input Values

--- SeoJangSeok/Vanilla-JS-chrome-app/app.js
+++ SeoJangSeok/Vanilla-JS-chrome-app/app.js
@@ -1,13 +1,10 @@
-const h1 = document.querySelector('div.hello:first-child h1') // hello라는 class를 가진 div 내부의 first-child인 h1
+const loginInput = document.querySelector('#login-form input')
+const loginButton = document.querySelector('#login-form button')
 
-function handleTitleClick() {
-  // const clickedClass = "clicked"
-  // if (h1.classList.contains(clickedClass)) {
-  //     h1.classList.remove(clickedClass)
-  //   } else {
-  //     h1.classList.add(clickedClass)
-  //   }
-  // }
-  h1.classList.toggle('clicked')
+function onLoginBtnClick() {
+  // console.dir(loginInput)
+  console.log('hello', loginInput.value)
+  // console.log('click!!!')
 }
-h1.addEventListener('click', handleTitleClick)
+
+loginButton.addEventListener('click', onLoginBtnClick)
