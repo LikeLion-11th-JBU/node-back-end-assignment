bf9cfbb2a5c4812c8dd06c222aac98414fec51ac
seo jang seok <seok7197@naver.com>
2023-04-21 02:32:43 +0900

    4.4-Getting Username

--- SeoJangSeok/Vanilla-JS-chrome-app/app.js
+++ SeoJangSeok/Vanilla-JS-chrome-app/app.js
@@ -1,18 +1,18 @@
 const loginForm = document.querySelector('#login-form')
 const loginInput = document.querySelector('#login-form input')
+const greeting = document.querySelector('#greeting')
 
-const link = document.querySelector('a')
+const HIDDEN_CLASSNAME = 'hidden' // 'hidden' class를 자주 쓰니까 변수 할당.
+// 일반적으로 string만 포함된 변수는 대문자로 표기하고 string을 저장하고 싶을 때 사용. loginForm이나, loginInput 처럼 중요한 정보를 담은게 아니라서 대문자로 작성.
 
 function onLoginSubmit(event) {
   event.preventDefault()
-  // const username = loginInput.value
-  console.log(loginInput.value)
-}
-
-function handleLinkClick(event) {
-  event.preventDefault()
-  console.dir(event)
+  const username = loginInput.value
+  loginForm.classList.add(HIDDEN_CLASSNAME)
+  console.log(username)
+  // greeting.innerText = 'Hello ' + username // 아래랑 같음.
+  greeting.innerText = `Hello ${username}` // 위에랑 같음.
+  greeting.classList.remove(HIDDEN_CLASSNAME)
 }
 
 loginForm.addEventListener('submit', onLoginSubmit)
-link.addEventListener('click', handleLinkClick)
