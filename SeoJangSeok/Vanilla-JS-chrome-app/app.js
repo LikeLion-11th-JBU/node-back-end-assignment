06d0d9f397977a24cc9a6f88a681c88fcaea78c9
seo jang seok <seok7197@naver.com>
2023-04-22 00:17:45 +0900

    4.6-Loading Username

--- SeoJangSeok/Vanilla-JS-chrome-app/app.js
+++ SeoJangSeok/Vanilla-JS-chrome-app/app.js
@@ -4,15 +4,30 @@ const greeting = document.querySelector('#greeting')
 
 const HIDDEN_CLASSNAME = 'hidden' // 'hidden' class를 자주 쓰니까 변수 할당.
 // 일반적으로 string만 포함된 변수는 대문자로 표기하고 string을 저장하고 싶을 때 사용. loginForm이나, loginInput 처럼 중요한 정보를 담은게 아니라서 대문자로 작성.
+const USERNAME_KEY = 'username' // 'username' string을 자주 쓰니까 변수 할당.
 
 function onLoginSubmit(event) {
   event.preventDefault()
   loginForm.classList.add(HIDDEN_CLASSNAME)
   const username = loginInput.value
-  localStorage.setItem('username', username)
-  // greeting.innerText = 'Hello ' + username // 아래랑 같음.
-  greeting.innerText = `Hello ${username}` // 위에랑 같음.
+  localStorage.setItem(USERNAME_KEY, username)
+  paintGreetings(username)
+}
+
+function paintGreetings(username) {
+  greeting.innerText = `Hello ${username}`
   greeting.classList.remove(HIDDEN_CLASSNAME)
 }
 
-loginForm.addEventListener('submit', onLoginSubmit)
+const savedUsername = localStorage.getItem(USERNAME_KEY)
+
+console.log(savedUsername) // null 값이 나오면 username이 저장되지 않았다는 뜻.
+
+if (savedUsername === null) {
+  // show the form
+  loginForm.classList.remove(HIDDEN_CLASSNAME)
+  loginForm.addEventListener('submit', onLoginSubmit)
+} else {
+  // show the greeting
+  paintGreetings(savedUsername)
+}
