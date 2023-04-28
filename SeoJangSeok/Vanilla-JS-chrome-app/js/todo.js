3ae5ced6d7be7382e2f2671ce2708d2870c76be8
seo jang seok <seok7197@naver.com>
2023-04-29 01:01:03 +0900

    7.1-Adding ToDo

--- SeoJangSeok/Vanilla-JS-chrome-app/js/todo.js
+++ SeoJangSeok/Vanilla-JS-chrome-app/js/todo.js
@@ -3,10 +3,19 @@ const toDoInput = toDoForm.querySelector('input')
 // const toDoInput = document.querySelector("#todo-form input")
 const toDoList = document.getElementById('todo-list')
 
+function paintToDo(newTodo) {
+  const li = document.createElement('Li')
+  const span = document.createElement('span')
+  li.appendChild(span)
+  span.innerText = newTodo
+  toDoList.appendChild(li)
+}
+
 function handleToDoSubmit(event) {
   event.preventDefault()
   const newTodo = toDoInput.value // input에 입력한 value를 새로운 변수에 복사.
   toDoInput.value = '' // 입력했을 때 칸 비워주기.
+  paintToDo(newTodo)
 }
 
 toDoForm.addEventListener('submit', handleToDoSubmit)
