4833dca22548766bcbb960c2f7cee6b6beb42e70
kimchaneil <chaneil2@naver.com>
2023-05-07 08:01:41 +0900

    DB데이터 불러오기 및 html에 DB데이터 적용

--- KimChanIl/codingapple/server.js
+++ KimChanIl/codingapple/server.js
@@ -2,6 +2,7 @@ const express = require('express'); //라이브 러리 참고
 const app = express(); // 라이브러리를 이용한 객체 생성
 const MongoClient = require('mongodb').MongoClient;
 const bodyParser = require('body-parser');
+app.set('view engine','ejs');
 app.use(bodyParser.urlencoded({extended : true}));
 //npm install mongodb할 때 강의 버전으로 해야돼..
 var db;
@@ -10,9 +11,6 @@ MongoClient.connect('mongodb+srv://Kchan:1234@codingapple.zlnpnec.mongodb.net/?r
     //서버띄우는 코드 여기로 옮기기
     db = client.db('todoapp');
 
-    db.collection('post').insertOne({_id:100,이름: 'John' , 나이: 20},function(error,result){
-        console.log('saved!');
-    });
     app.listen('8080', function(){
       console.log('listening on 8080')
     });
@@ -40,3 +38,11 @@ app.post('/add',function(req,res){
     db.collection('post').insertOne({_id:101 ,날짜:req.body.date, 기한:req.body.title},function(error,result){
         console.log('saved!');});
 });
+
+app.get('/list',function(req,res){
+    db.collection('post').find().toArray(function(error,result){
+        console.log(result);
+        res.render('list.ejs',{posts : result});
+    });
+});
+
