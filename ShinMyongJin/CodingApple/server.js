237342ae17032f2cee5faca1d816c0b5a75a8b09
Myongjin <audwls714@gmail.com>
2023-05-13 00:17:32 +0900

    nodemon 자동화

--- ShinMyongJin/CodingApple/server.js
+++ ShinMyongJin/CodingApple/server.js
@@ -15,3 +15,7 @@ app.get('/pet', function (요청, 응답) {
 app.get('/beauty', function (요청, 응답) {
   응답.send('뷰티용품 쇼핑페이지여');
 });
+
+app.get('/', function (요청, 응답) {
+  응답.sendFile(__dirname + '/index.html');
+});
