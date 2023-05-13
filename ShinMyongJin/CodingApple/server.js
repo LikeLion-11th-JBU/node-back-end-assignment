f1217a5f177dfef02bbcf810bec98b4aa312ac00
Myongjin <audwls714@gmail.com>
2023-05-13 15:46:13 +0900

    MongoDB 셋팅

--- ShinMyongJin/CodingApple/server.js
+++ ShinMyongJin/CodingApple/server.js
@@ -1,5 +1,17 @@
 const express = require('express');
 const app = express();
+const MongoClient = require('mongodb').MongoClient;
+
+MongoClient.connect(
+  'mongodb+srv://MJ00:myongjin714@mj00.anmafpi.mongodb.net/?retryWrites=true&w=majority',
+  function (에러, client) {
+    if (에러) return console.log(에러);
+    //서버띄우는 코드 여기로 옮기기
+    app.listen('8080', function () {
+      console.log('listening on 8080');
+    });
+  }
+);
 
 app.listen(8080, function () {
   console.log('listening on 8080');
@@ -11,11 +23,6 @@ app.listen(8080, function () {
 app.get('/pet', function (요청, 응답) {
   응답.send('펫용품 쇼핑할 수 있는 페이지입니다.');
 });
-
-app.get('/beauty', function (요청, 응답) {
-  응답.send('뷰티용품 쇼핑페이지여');
-});
-
 app.get('/', function (요청, 응답) {
   응답.sendFile(__dirname + '/index.html');
 });
