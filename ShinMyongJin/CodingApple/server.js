2d8bcd21dc6066c1ee4785ac6e61f96748dc0264
Myongjin <audwls714@gmail.com>
2023-05-13 22:20:24 +0900

    중간 업로드

--- ShinMyongJin/CodingApple/server.js
+++ ShinMyongJin/CodingApple/server.js
@@ -2,33 +2,50 @@ const express = require('express');
 const app = express();
 const MongoClient = require('mongodb').MongoClient;
 
+var db;
 MongoClient.connect(
-  'mongodb+srv://MJ00:myongjin714@mj00.anmafpi.mongodb.net/?retryWrites=true&w=majority',
+  'mongodb+srv://audwls714:myongjin714@mj00.anmafpi.mongodb.net/?retryWrites=true&w=majority',
+  { useUnifiedTopology: true },
   function (에러, client) {
     if (에러) return console.log(에러);
-    //서버띄우는 코드 여기로 옮기기
-    app.listen('8080', function () {
+    db = client.db('todoapp');
+
+    app.listen(8080, function () {
       console.log('listening on 8080');
     });
   }
 );
-
-app.listen(8080, function () {
-  console.log('listening on 8080');
-});
-
-//누군가 /pet으로 방문을 하면
-// pet 관련된 안내문을 띄우기
-
-app.get('/pet', function (요청, 응답) {
-  응답.send('펫용품 쇼핑할 수 있는 페이지입니다.');
-});
 app.get('/', function (요청, 응답) {
   응답.sendFile(__dirname + '/index.html');
 });
 
 app.get('/write', function (요청, 응답) {
   응답.sendFile(__dirname + '/write.html');
+});
+
+app.get('/list', function (req, res) {
+  db.collection('post')
+    .find()
+    .toArray(function (error, result) {
+      console.log(result);
+      res.render('list.ejs', { posts: result });
+    });
+});
+
+app.post('/add', function (요청, 응답) {
+  db.collection('counter').findOne(
+    { name: '게시물갯수' },
+    function (에러, 결과) {
+      var 총게시물갯수 = 결과.totalPost;
+      db.collection('post').insertOne(
+        { _id: 총게시물갯수 + 1, 제목: 요청.body.title, 날짜: 요청.body.date },
+        function () {
+          console.log('저장완료');
+          응답.send('전송완료');
+        }
+      );
+    }
+  );
 });
 
 app.use(express.urlencoded({ extended: true }));
