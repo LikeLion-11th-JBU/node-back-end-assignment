f31261071d22ecda4b56b4ea81b84d46224fb7a8
Myongjin <audwls714@gmail.com>
2023-05-14 23:05:50 +0900

    HTML 파일구성

--- ShinMyongJin/CodingApple/server.js
+++ ShinMyongJin/CodingApple/server.js
@@ -37,11 +37,20 @@ app.post('/add', function (요청, 응답) {
     { name: '게시물갯수' },
     function (에러, 결과) {
       var 총게시물갯수 = 결과.totalPost;
+
       db.collection('post').insertOne(
         { _id: 총게시물갯수 + 1, 제목: 요청.body.title, 날짜: 요청.body.date },
-        function () {
-          console.log('저장완료');
-          응답.send('전송완료');
+        function (에러, 결과) {
+          db.collection('counter').updateOne(
+            { name: '게시물갯수' },
+            { $inc: { totalPost: 1 } },
+            function (에러, 결과) {
+              if (에러) {
+                return console.log(에러);
+              }
+              응답.send('전송완료');
+            }
+          );
         }
       );
     }
