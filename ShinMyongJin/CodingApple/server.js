d4c0bfba7222307fb77cb5015b3c0a723d19bf4f
Myongjin <audwls714@gmail.com>
2023-05-27 15:29:05 +0900

    [ADD] 채팅방 만들기

--- ShinMyongJin/CodingApple/server.js
+++ ShinMyongJin/CodingApple/server.js
@@ -3,6 +3,7 @@ const app = express();
 const MongoClient = require('mongodb').MongoClient;
 const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
+const { ObjectId } = require('mongodb');
 app.use(methodOverride('_method'));
 
 app.set('view engine', 'ejs');
@@ -259,4 +260,28 @@ app.post('/upload', upload.single('MJ'), function (요청, 응답) {
 
 app.get('/image/:imageName', function (요청, 응답) {
   응답.sendFile(__dirname + '/public/image/' + 요청.params.imageName);
+});
+
+app.post('/chatroom', function (요청, 응답) {
+  var 저장할거 = {
+    title: '무슨무슨채팅방',
+    member: [ObjectId(요청.body.당한사람id), 요청.user._id],
+    date: new Date(),
+  };
+
+  db.collection('chatroom')
+    .insertOne(저장할거)
+    .then(function (결과) {
+      응답.send('저장완료');
+    });
+});
+
+app.get('/chat', 로그인했니, function (요청, 응답) {
+  db.collection('chatroom')
+    .find({ member: 요청.user._id })
+    .toArray()
+    .then((결과) => {
+      console.log(결과);
+      응답.render('chat.ejs', { data: 결과 });
+    });
 });
