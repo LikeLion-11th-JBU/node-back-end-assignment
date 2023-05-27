e664709b92c3018f3344dfc76baa083fa9a3779a
Myongjin <audwls714@gmail.com>
2023-05-27 16:25:18 +0900

    [ADD] 채팅방 메세지 발행

--- ShinMyongJin/CodingApple/server.js
+++ ShinMyongJin/CodingApple/server.js
@@ -285,3 +285,19 @@ app.get('/chat', 로그인했니, function (요청, 응답) {
       응답.render('chat.ejs', { data: 결과 });
     });
 });
+
+app.post('/message', 로그인했니, function (요청, 응답) {
+  var 저장할거 = {
+    parent: 요청.body.parent,
+    content: 요청.body.content,
+    userid: 요청.user._id,
+    date: new Date(),
+  };
+
+  db.collection('message')
+    .insertOne(저장할거)
+    .then(() => {
+      console.log('DB저장성공');
+      응답.send('DB저장성공');
+    });
+});
