a68f4d29295a3030603345494c47572625d6d7ce
Myongjin <audwls714@gmail.com>
2023-07-06 16:01:40 +0900

    [ADD]readFile

--- ShinMyongJin/TDDNodejs/index.js
+++ ShinMyongJin/TDDNodejs/index.js
@@ -1,5 +1,7 @@
 const fs = require('fs');
 
-const data = fs.readFileSync('./data.txt', 'utf8');
+// const data = fs.readFileSync('./data.txt', 'utf8');
 
-console.log(data);
+const data = fs.readFile('./data.txt', 'utf8', function (err, data) {
+  console.log(data);
+});
