67b4c4daa8a68bc99cc430cda7e66f6d33d8c382
Myongjin <audwls714@gmail.com>
2023-07-11 23:45:52 +0900

    [ADD] 테스트 환경 개선

--- ShinMyongJin/TDDNodejs/node-api/index.js
+++ ShinMyongJin/TDDNodejs/node-api/index.js
@@ -4,13 +4,11 @@ var morgan = require('morgan');
 var bodyParser = require('body-parser');
 var user = require('./api/user');
 
-app.use(morgan('dev'));
+if (process.env.NODE_ENV !== 'test') {
+  app.use(morgan('dev'));
+}
 app.use(express.json()); // for parsing application/json
 app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 app.use('/users', user);
-
-app.listen(3000, function () {
-  console.log(`Example app listening on port 3000!`);
-});
 
 module.exports = app;
