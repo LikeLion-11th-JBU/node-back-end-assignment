c70882d8f628ae478a59a5f2a39f3ea290376acf
Myongjin <audwls714@gmail.com>
2023-07-11 16:21:08 +0900

    [ADD] 사용자 추가 API 실패시

--- ShinMyongJin/TDDNodejs/node-api/index.spec.js
+++ ShinMyongJin/TDDNodejs/node-api/index.spec.js
@@ -48,7 +48,7 @@ describe('GET /user1은', () => {
     });
   });
 });
-describe('GET /users/1', () => {
+describe('DELETE /users/1', () => {
   describe('성공시', () => {
     it('204를 응답한다', (done) => {
       request(app).delete('/users/1').expect(204).end(done);
@@ -79,6 +79,18 @@ describe('POST /users', () => {
     });
     it('입력한 name을 반환한다', () => {
       body.should.have.property('name', name);
+    });
+  });
+  describe('실패시', () => {
+    it('name 파라미터 누락시 400를 반환한다', (done) => {
+      request(app).post('/users').send({}).expect(400).end(done);
+    });
+    it('name이 중복일 경우 409를 반환한다', (done) => {
+      request(app)
+        .post('/users')
+        .send({ name: 'daniel' })
+        .expect(409)
+        .end(done);
     });
   });
 });
