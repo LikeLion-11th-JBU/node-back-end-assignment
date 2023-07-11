9ff3a1a07908be4d97ab967045c2eb020a3c39d3
Myongjin <audwls714@gmail.com>
2023-07-11 17:22:30 +0900

    [ADD] 사용자 수정API 실패시

--- ShinMyongJin/TDDNodejs/node-api/index.js
+++ ShinMyongJin/TDDNodejs/node-api/index.js
@@ -52,9 +52,17 @@ app.post('/users', (req, res) => {
 
 app.put('/users/:id', (req, res) => {
   const id = parseInt(req.params.id, 10);
+  if (Number.isNaN(id)) return res.status(400).end();
+
   const name = req.body.name;
+  if (!name) return res.status(400).end();
+
+  const isConflic = users.filter((user) => user.name === name).length;
+  if (isConflic) return res.status(409).end();
 
   const user = users.filter((user) => user.id === id)[0];
+  if (!user) return res.status(404).end();
+
   user.name = name;
 });
 
