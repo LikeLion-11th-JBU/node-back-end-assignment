deda1e7b424f04d9695c7f6c402aeb2f0d3e0c6b
seo jang seok <seok7197@naver.com>
2023-07-13 01:36:42 +0900

    [ADD] 사용자 목록 조회 API 테스트 코드 만들기

--- SeoJangSeok/TDD/NODE_API/index.js
+++ SeoJangSeok/TDD/NODE_API/index.js
@@ -1,8 +1,10 @@
 // 사용자 목록 조회 API
-var express = require('express')
-var app = express()
-var morgan = require('morgan')
-var users = [
+const express = require('express')
+const app = express()
+const port = 3000
+const morgan = require('morgan')
+
+const users = [
   { id: 1, name: 'alice' },
   { id: 2, name: 'bek' },
   { id: 3, name: 'chris' },
@@ -11,11 +13,16 @@ var users = [
 app.use(morgan('dev'))
 
 app.get('/users', function (req, res) {
-  res.json('users')
+  req.query.limit = req.query.limit || 10
+  const limit = parseInt(req.query.limit, 10)
+  if (Number.isNaN(limit)) {
+    return res.status(400).end()
+  }
+  res.json(users.slice(0, limit))
 })
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+app.listen(port, function () {
+  console.log('Example app listening on port ${port}')
 })
 
 module.exports = app
