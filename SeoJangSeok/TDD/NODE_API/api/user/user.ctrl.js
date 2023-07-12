63cac797706fb19c0d7fa85d1f8513a3b49c0f69
seo jang seok <seok7197@naver.com>
2023-07-13 03:54:27 +0900

    [ADD] DB와 show 컨트롤러 연동

--- SeoJangSeok/TDD/NODE_API/api/user/user.ctrl.js
+++ SeoJangSeok/TDD/NODE_API/api/user/user.ctrl.js
@@ -1,25 +1,28 @@
 // api 로직
 const models = require('../../models')
-
 const index = function (req, res) {
   req.query.limit = req.query.limit || 10
   const limit = parseInt(req.query.limit, 10) //"2"라는 문자열로 들어온다 => parseInt(인자값,진수)를 사용해 정수로 변환
   if (Number.isNaN(limit)) {
     return res.status(400).end()
   }
-
   models.User.findAll({ limit: limit }).then((users) => {
     res.json(users)
   })
 }
-
 const show = function (req, res) {
   const id = parseInt(req.params.id, 10)
   if (Number.isNaN(id)) return res.status(400).end()
-  const user = users.filter((user) => user.id === id)[0]
-  if (!user) return res.status(404).end()
-  res.json(user)
+  models.User.findOne({
+    where: {
+      id: id,
+    },
+  }).then((user) => {
+    if (!user) return res.status(404).end()
+    res.json(user)
+  })
 }
+
 const destroy = function (req, res) {
   const id = parseInt(req.params.id, 10)
   if (Number.isNaN(id)) return res.status(400).end()
