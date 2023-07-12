b8677db880b79cb10317dfc7181c930feebb8d22
seo jang seok <seok7197@naver.com>
2023-07-12 23:56:16 +0900

    [ADD] 어플리케이션

--- SeoJangSeok/TDD/NODE_API/index.js
+++ SeoJangSeok/TDD/NODE_API/index.js
@@ -1,24 +1,6 @@
-// 노드의 Hello World 서버 코드
-const http = require('http') // http 모듈 가져옴
+const express = require('express')
+const app = express()
 
-const hostname = '127.0.0.1' // 내 컴퓨터
-const port = 3000 // 포트번호
-
-const server = http.createServer((req, res) => {
-  if (req.url === '/') {
-    res.statusCode = 200
-    res.setHeader('Content-Type', 'text/plain')
-    res.end('Hello World\n')
-  } else if (req.url === '/users') {
-    res.statusCode = 200
-    res.setHeader('Content-Type', 'text/plain')
-    res.end('User list')
-  } else {
-    res.statusCode = 404
-    res.end('Not Found')
-  }
-})
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`)
+app.listen(3000, function () {
+  console.log('Server is running')
 })
