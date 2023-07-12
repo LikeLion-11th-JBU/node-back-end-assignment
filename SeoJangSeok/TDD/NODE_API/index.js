db6bb357dc9612b8e288a72669608ea1a8a9de0c
seo jang seok <seok7197@naver.com>
2023-07-12 23:54:24 +0900

    [ADD] 라우팅 추가

--- SeoJangSeok/TDD/NODE_API/index.js
+++ SeoJangSeok/TDD/NODE_API/index.js
@@ -1,13 +1,22 @@
 // 노드의 Hello World 서버 코드
-const http = require('http')
+const http = require('http') // http 모듈 가져옴
 
-const hostname = '127.0.0.1'
-const port = 3000
+const hostname = '127.0.0.1' // 내 컴퓨터
+const port = 3000 // 포트번호
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200
-  res.setHeader('Content-Type', 'text/plain')
-  res.end('Hello World\n')
+  if (req.url === '/') {
+    res.statusCode = 200
+    res.setHeader('Content-Type', 'text/plain')
+    res.end('Hello World\n')
+  } else if (req.url === '/users') {
+    res.statusCode = 200
+    res.setHeader('Content-Type', 'text/plain')
+    res.end('User list')
+  } else {
+    res.statusCode = 404
+    res.end('Not Found')
+  }
 })
 
 server.listen(port, hostname, () => {
