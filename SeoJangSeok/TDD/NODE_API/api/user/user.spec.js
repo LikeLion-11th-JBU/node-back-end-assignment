9e63cc620123d3e7bc6fee9306c79fb4110cf5a0
seo jang seok <seok7197@naver.com>
2023-07-13 03:11:47 +0900

    [ADD] 테스트 환경 개선

--- SeoJangSeok/TDD/NODE_API/api/user/user.spec.js
+++ SeoJangSeok/TDD/NODE_API/api/user/user.spec.js
@@ -1,11 +1,10 @@
-// 테스트 코드
+const app = require('../../index')
 const request = require('supertest')
 const should = require('should')
-const app = require('../../index')
 
 describe('GET /users는', () => {
-  describe('성공 시', () => {
-    it('유저 객체를 담은 배열로 응답한다.', (done) => {
+  describe('성공시', () => {
+    it('유저 객체를 담은 배열로 응답한다', (done) => {
       request(app)
         .get('/users')
         .end((err, res) => {
@@ -13,7 +12,7 @@ describe('GET /users는', () => {
           done()
         })
     })
-    it('최대 limit 개수만큼 응답한다.', (done) => {
+    it('최대 limit 갯수만큼 응답한다', (done) => {
       request(app)
         .get('/users?limit=2')
         .end((err, res) => {
@@ -22,15 +21,15 @@ describe('GET /users는', () => {
         })
     })
   })
-  describe('실패 시', () => {
-    it('limit이 숫자형이 아니면 400을 응답한다.', (done) => {
+  describe('실패시', () => {
+    it('limit이 숫자형이 아니면 400을 응답한다', (done) => {
       request(app).get('/users?limit=two').expect(400).end(done)
     })
   })
 })
-describe('GET /users/:id는', () => {
-  describe('성공 시', () => {
-    it('id가 1인 유저 객체를 반환한다.', (done) => {
+describe('GET /users/:id', () => {
+  describe('성공시', () => {
+    it('id가 1인 유저 객체를 반환한다', (done) => {
       request(app)
         .get('/users/1')
         .end((err, res) => {
@@ -39,8 +38,8 @@ describe('GET /users/:id는', () => {
         })
     })
   })
-  describe('실패 시', () => {
-    it('id가 숫자가 아닐 경우 400으로 응답한다.', (done) => {
+  describe('실패시', () => {
+    it('id가 숫자가 아닐경우 400으로 응답한다', (done) => {
       request(app).get('/users/one').expect(400).end(done)
     })
     it('id로 유저를 찾을 수 없을 경우 404로 응답한다.', (done) => {
@@ -49,19 +48,19 @@ describe('GET /users/:id는', () => {
   })
 })
 describe('DELETE /users/:id', () => {
-  describe('성공 시', () => {
-    it('204를 응답한다.', (done) => {
+  describe('성공시', () => {
+    it('204를 응답한다', (done) => {
       request(app).delete('/users/1').expect(204).end(done)
     })
   })
-  describe('실패 시', () => {
-    it('id가 숫자가 아닐 경우 400으로 응답한다.', (done) => {
+  describe('실패시', () => {
+    it('id가 숫자가 아닐경우 400으로 응답한다', (done) => {
       request(app).delete('/users/one').expect(400).end(done)
     })
   })
 })
 describe('POST /users', () => {
-  describe('성공 시', () => {
+  describe('성공시', () => {
     let name = 'daniel',
       body
     before((done) => {
@@ -74,25 +73,25 @@ describe('POST /users', () => {
           done()
         })
     })
-    it('생성된 유저 객체를 반환한다.', () => {
+    it('생성된 유저 객체를 반환한다', () => {
       body.should.have.property('id')
     })
-    it('입력한 name을 반환한다.', () => {
+    it('입력한 name을 반환한다', () => {
       body.should.have.property('name', name)
     })
   })
-  describe('실패 시', () => {
-    it('name 파라미터 누락 시 400을 반환한다.', (done) => {
+  describe('실패시', () => {
+    it('name 파라매터를 누락시 400을 반환한다', (done) => {
       request(app).post('/users').send({}).expect(400).end(done)
     })
-    it('name이 중복일 경우 409를 반환한다.', (done) => {
+    it('name이 중복일 경우 409를 반환한다', (done) => {
       request(app).post('/users').send({ name: 'daniel' }).expect(409).end(done)
     })
   })
 })
-describe('PUT /users/:id', () => {
-  describe('성공 시', () => {
-    it('변경된 name을 응답한다.', (done) => {
+describe('PUT /users:id', () => {
+  describe('성공시', () => {
+    it('변경된 name을 응답한다', (done) => {
       const name = 'chally'
       request(app)
         .put('/users/3')
@@ -103,17 +102,19 @@ describe('PUT /users/:id', () => {
         })
     })
   })
-  describe('실패 시', () => {
-    it('정수가 아닌 id일 경우 400을 응답한다.', (done) => {
+  describe('실패시', () => {
+    it('정수가 아닌 id일 경우 400을 응답한다', (done) => {
       request(app).put('/users/one').expect(400).end(done)
     })
-    it('name이 없을 경우 400을 응답한다.', (done) => {
+    it('name이 없을 경우 400을 응답한다', (done) => {
       request(app).put('/users/1').send({}).expect(400).end(done)
     })
-    it('없는 유저일 경우 404를 응답한다.', (done) => {
+
+    it('없는 유저일 경우 404을 응답한다', (done) => {
       request(app).put('/users/999').send({ name: 'foo' }).expect(404).end(done)
     })
-    it('이름이 중복일 경우 409를 응답한다.', (done) => {
+
+    it('이름이 중복일 409을 응답한다', (done) => {
       request(app).put('/users/3').send({ name: 'bek' }).expect(409).end(done)
     })
   })
--- SeoJangSeok/TDD/NODE_API/index.js
+++ SeoJangSeok/TDD/NODE_API/index.js
@@ -1,19 +1,17 @@
 // 사용자 목록 조회 API
 const express = require('express')
 const app = express()
-const port = 3000
 const morgan = require('morgan')
 const bodyParser = require('body-parser')
-const user = require('./api/user/index')
+const user = require('./api/user')
 
-app.use(morgan('dev'))
+if (process.env.NODE_ENV !== 'test') {
+  app.use(morgan('dev'))
+}
+
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
 app.use('/users', user)
-
-app.listen(port, function () {
-  console.log('Example app listening on port ${port}')
-})
 
 module.exports = app
